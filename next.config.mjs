b9bd0e1a7763c3d42d00603a51c979b3fac2da09
HOUEHA Karen <houehakaren@gmail.com>
2026-03-20 15:30:42 +0100

    Corrections

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export', // Active l'export statique
+  output: 'export', // Active l'export statique
   trailingSlash: true,
   images: {
     unoptimized: true, // Nécessaire pour l'export statique
