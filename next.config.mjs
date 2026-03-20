1549c8447537b1c8eb09d88a0c0305408f4c48ed
Achabi Abogourin <achabiabogourin77@gmail.com>
2026-03-20 20:24:23 +0100

    solutionv1

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  ...(process.env.STATIC_EXPORT === 'true' && { output: 'export' }),
   trailingSlash: true,
   images: {
     unoptimized: true,
